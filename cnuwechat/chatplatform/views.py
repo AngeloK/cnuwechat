a5d6f9ba87e61e1b71b672704157c648add8ad4f
Neyanbhbin <neyanbhbin.kang@gmail.com>
2015-03-20 10:37:07 +0800

    Init cnuwechat

---
+++ cnuwechat/chatplatform/views.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import hashlib
+
+# Create your views here.
+
+def checkSignture(request):
+	'''
+	Check signature 
+	'''
+	signature = request.GET['signature']
+	timestamp = request.GET['timestamp']
+	nonce = request.GET['nonce']
+	echostr = request.GET['echostr']
+
+	token = 'kangyongjie'
+
+	sorted_str = sorted([token,timestamp,nonce])
+	result_str = str(sorted_str[0]+sorted_str[1]+sorted_str[2])				
+
+	result = hashlib.sha1(result_str).hexdigest()
+
+	if result == signature:
+		return HttpResponse(echostr)
+	else:
+		return HttpResponse("not match")
+
+
+def receiveMsg(request):
+	'''
+	receive message from wechat server
+	'''
+
+	data = fromstring(request.body)
+
+	tag_list = [item.tag for item in data]
+	text_list = [item.text for item in data]
+
+	replyMsg = dict(zip(tag_list,text_list))
+
+	response = JsonResponse(replyMsg)
+
+def main(request):
+
+	if request.method == 'GET':
+		return checkSignture(request)
+	elif request.method == 'POST':
+		return receiveMsg(request)
+	else:
+		return Http404
